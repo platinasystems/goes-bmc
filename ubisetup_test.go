5c9338bc725d6c1bbdc0013f320fe7e118aa120d
Kevin Paul Herbert <kph@platinasystems.com>
2021-05-03 18:34:46 -0700

    ubisetup_test.go: Fix test
    Properly use the Example... test feature.
    
    Signed-off-by: Kevin Paul Herbert <kph@platinasystems.com>

--- ubisetup_test.go
+++ ubisetup_test.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
-	"testing"
 )
 
-func TestIpCommand(t *testing.T) {
-	s := ipCommand([]byte("172.17.3.52::172.17.2.1:255.255.254.0::eth0"))
+func Example_ipCommand() {
+	s := ipCommand("172.17.3.52::172.17.2.1:255.255.254.0::eth0")
 	fmt.Println(string(s))
+	// Output:
+	// ip link eth0 change up
+	// ip address add 172.17.3.52/23 dev eth0
+	// ip route add 0.0.0.0/0 via 172.17.2.1
 }
